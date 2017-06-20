1ddc9a1f2c4a9a1d7385e403ad7f3a486565c691
vicki alden <vic.alden23@gmail.com>
2017-06-20 14:20:45 -0700

    Add picture schema to db

---
+++ talk-about-me/db/Picture.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/talkaboutmedb');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('CONNETION TO talkaboutmedb IS OPEN')
+})
+
+var picSchema = new mongoose.Schema({
+  image: {type: String, required:true},
+  name: {type: String, required:true},
+  age: {type: Number},
+  quote: {type: String},
+  hot: {type: Number},
+  regular: {type: Number},
+  not: {type: Number}
+})
+
+var Picture = mongoose.model('Picture', catSchema);
+
+module.exports = Cat;
