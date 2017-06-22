312aa8360fc19b1adadeaa3cd4f4f762fc21febe
vicki alden <vic.alden23@gmail.com>
2017-06-21 17:10:19 -0700

    Create skeleton for landing page

--- talk-about-me/client/TalkAboutMe.js
+++ talk-about-me/client/TalkAboutMe.js
@@ -1,1 +1,20 @@
 import React, { Component } from 'react';
+
+class TalkAboutMe extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+    );
+  };
+};
+
+export default TalkAboutMe;
