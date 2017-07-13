9baa5f85cb3895209b67b39d42cacfa08e5024ae
vicki alden <vic.alden23@gmail.com>
2017-07-13 14:16:29 -0700

    Add style sheet for semantic UI

--- talk-about-me/src/client/PhotoForm.js
+++ talk-about-me/src/client/PhotoForm.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
-
-const options = [
-  { key: 'm', text: 'Male', value: 'male' },
-  { key: 'f', text: 'Female', value: 'female' },
-];
+import { Button, Checkbox, Form } from 'semantic-ui-react';
 
 class PhotoForm extends Component {
   constructor(props) {
@@ -14,26 +9,21 @@ class PhotoForm extends Component {
     };
   }
 
-  handleChange = (e, { value }) => this.setState({ value })
-
   render() {
-    const { value } = this.state
     return (
       <Form>
-        <Form.Group widths='equal'>
-          <Form.Input label='First name' placeholder='First name' />
-          <Form.Input label='Last name' placeholder='Last name' />
-          <Form.Select label='Gender' options={options} placeholder='Gender' />
-        </Form.Group>
-        <Form.Group inline>
-          <label>Size</label>
-          <Form.Radio label='Small' value='sm' checked={value === 'sm'} onChange={this.handleChange} />
-          <Form.Radio label='Medium' value='md' checked={value === 'md'} onChange={this.handleChange} />
-          <Form.Radio label='Large' value='lg' checked={value === 'lg'} onChange={this.handleChange} />
-        </Form.Group>
-        <Form.TextArea label='About' placeholder='Tell us more about you...' />
-        <Form.Checkbox label='I agree to the Terms and Conditions' />
-        <Form.Button>Submit</Form.Button>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder='First Name' />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder='Last Name' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='I agree to the Terms and Conditions' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
       </Form>
     )
   }
