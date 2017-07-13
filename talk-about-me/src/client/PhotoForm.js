97a806c8887c42a231561cf6cd02fadec29934ad
vicki alden <vic.alden23@gmail.com>
2017-07-13 16:05:46 -0700

    Add handleSubmit function to form

--- talk-about-me/src/client/PhotoForm.js
+++ talk-about-me/src/client/PhotoForm.js
@@ -6,19 +6,36 @@ class PhotoForm extends Component {
     super(props);
 
     this.state = {
+      name: '',
+      url: '',
+      age: 0,
+      quote: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.name,
+      url: event.target.url});
+  }
+
+  handleSubmit() {
+    console.log(this.state.url);
   }
 
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <Form.Field>
           <label>Name</label>
-          <input placeholder='Name' />
+          <input placeholder='Name' type="text" name={this.state.name} onChange={this.handleChange} />
         </Form.Field>
         <Form.Field>
           <label>Image URL</label>
-          <input placeholder='www.image.com' />
+          <input placeholder='www.image.com' type="text" url={this.state.url} onChange={this.handleChange}/>
         </Form.Field>
         <Form.Field>
           <label>Age</label>
